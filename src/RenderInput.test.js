2dea996edc95561df81c2e0cd815a1df1bd9c7f2
React Mastering <pga.adam.scott@gmail.com>
2022-04-25 17:11:32 +0900

    init

---
+++ src/RenderInput.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import RenderInput from "./RenderInput";
+
+afterEach(() => cleanup());
+
+describe("Rendering", () => {
+  it("Should render all the elements correctly", () => {
+    render(<RenderInput />);
+    expect(screen.getByRole("button")).toBeTruthy();
+    expect(screen.getByPlaceholderText("Enter")).toBeTruthy();
+  });
+});
+
+describe("Input form onChange event", () => {
+  it("Should update input value correctly", async () => {
+    //const user = userEvent.setup();
+    render(<RenderInput />);
+    const inputValue = screen.getByPlaceholderText("Enter");
+    //await user.type(inputValue, "test");
+    await userEvent.type(inputValue, "test");
+    expect(inputValue.value).toBe("test");
+  });
+});
+
+describe("Console button conditionally triggered", () => {
+  it("Should not trigger output function", async () => {
+    const outputConsole = jest.fn();
+    render(<RenderInput outputConsole={outputConsole} />);
+    await userEvent.click(screen.getByRole("button"));
+    expect(outputConsole).not.toHaveBeenCalled();
+  });
+  it("Should trigger output function", async () => {
+    const outputConsole = jest.fn();
+    render(<RenderInput outputConsole={outputConsole} />);
+    const inputValue = screen.getByPlaceholderText("Enter");
+    await userEvent.type(inputValue, "test");
+    await userEvent.click(screen.getByRole("button"));
+    expect(outputConsole).toHaveBeenCalledTimes(1);
+  });
+});
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -5,7 +5,6 @@ import {
   increment,
   incrementByAmount,
   incrementAsync,
-  incrementIfOdd,
   selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
@@ -15,19 +14,9 @@ export function Counter() {
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
-  const incrementValue = Number(incrementAmount) || 0;
-
   return (
     <div>
       <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
         <button
           className={styles.button}
           aria-label="Increment value"
@@ -35,31 +24,35 @@ export function Counter() {
         >
           +
         </button>
+        <span className={styles.value}>{count}</span>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
       </div>
       <div className={styles.row}>
         <input
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
+          onChange={e => setIncrementAmount(e.target.value)}
         />
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
+          onClick={() =>
+            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+          }
         >
           Add Amount
         </button>
         <button
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
+          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
         >
           Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
         </button>
       </div>
     </div>
